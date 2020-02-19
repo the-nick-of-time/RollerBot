61e3062165bb24b47d5dd0807890beb61b073f2b
Nick Thurmes <nthurmes@gmail.com>
2020-02-19 00:57:40 -0700

    Use bot commands because that's what is really happening

--- rollerbot.py
+++ rollerbot.py
@@ -1,36 +1,26 @@
 #!/usr/bin/env python3
 
-import re
-import asyncio
-
-import discord
+from discord.ext import commands
 
 import dndice
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
 
 
-@client.event
-@asyncio.coroutine
-def on_ready():
+@bot.event
+async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
 
-@client.event
-@asyncio.coroutine
-def on_message(message: discord.Message):
-    m = re.match(r'/(roll|r)\s*(.*)', message.content)
-    if m:
-        results = []
-        for expr in m.group(2).split():
-            results.append(dndice.verbose(expr))
-        result = '; '.join(results)
-        yield from message.channel.send(result)
+@bot.command()
+async def roll(context: commands.Context, *args):
+    results = [dndice.verbose(expr) for expr in args]
+    await context.send('; '.join(results))
 
 
 with open('./app_token', 'r') as f:
     token = f.read().strip()
-client.run(token)
+bot.run(token)
